ae0c9366fba197fd397db66ef872d6b165e0905c
Carlo Dellarossa <dellarossa-p@hotmail.com>
2018-04-23 16:50:29 -0300

    Add files via upload

---
+++ application/modules/test/controllers/Test.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends MX_Controller {
+
+	public function index()
+	{
+	echo "<h1> Test mx </h1>";
+	}
+
+
+	public function hola(){
+		echo "<h1> Hola </h1>";
+	}
+}
