2eaf5caa8cd269fbb7f914d0ce74445cdb241d4b
carloDellarossa <dellarossa-p@hotmail.com>
2018-04-26 09:22:14 -0300

    push from vs

---
+++ application/modules/Templates/controllers/Categorias.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Categorias extends MX_Controller {
+
+    function __contruct(){
+        parent::__contruct();
+    }
+
+}
